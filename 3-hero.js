6869bc21dc29f27f82d334ddd8337d06965e5576
muanar.gadafi <muanar.gadafi@tonjoo.com>
2024-02-25 17:45:52 +0700

    day 3/366

---
+++ 3-hero.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// function hero(bullets, dragons){
+//     return bullets / 2 >= dragons;
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(100, 40));
